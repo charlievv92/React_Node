d8d6c942bb4f7ef952488fdde266debcd88d94de
huiju <dghs1233@gmail.com>
2024-11-05 15:57:55 +0900

    template 렌더링해보기

--- board_front/src/App.js
+++ board_front/src/App.js
@@ -1,24 +1,12 @@
 import logo from "./logo.svg";
 import "./App.css";
+import Dashboard from "./dashboard/Dashboard";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <h1>아 그렇게하는거 아닌데.</h1>
+    <div>
+      
+      <Dashboard/>
     </div>
   );
 }
