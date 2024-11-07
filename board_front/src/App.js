656c2d434525776442e4647a42cd1bbac9e1bfb8
Woocheol Kim <woocheolkim92@gmail.com>
2024-11-07 17:27:27 +0900

    컴포넌트 정리 및 react-router-dom을 활용한 네비게이션 기능 추가를 위한 설정 작업 중

--- board_front/src/App.js
+++ board_front/src/App.js
@@ -1,6 +1,8 @@
 import "./App.css";
 // import Dashboard from "./dashboard/Dashboard";
 import Board from "./dashboard/Board";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// import MenuContent from "./MenuContent";
 
 function App() {
   return (
