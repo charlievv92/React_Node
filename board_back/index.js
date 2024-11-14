a06b3d5d369023eb4acb9f5008b4d1084e6bab3d
dghs1233 <dghs1233@gmail.com>
2024-11-14 15:58:56 +0900

    db테스트확인

--- board_back/index.js
+++ board_back/index.js
@@ -18,6 +18,21 @@ app.get("/", (req, res) => {
   console.log("Request received");
 });
 
+// TODO: DB테스트는완료 하지만 index외 다른곳에서 export해야하는지 확인할것, passport로 로그인 작업하기 회원가입은 그후에,
+app.get("/testmys", (req, res) => {
+  const sqlQuery = "INSERT INTO board VALUES (6,'뭐지','ㅋㅋ',0,'흐림','2024-11-24',NULL,false,'aaa@aaa.com','192.000.111.111',NULL)";
+  
+  db.query(sqlQuery, (err, result) => {
+    if (err) {
+      console.log(err);
+      return res.status(500).send("Database error");
+    }
+
+    console.log('DB INSERT');
+    res.send("Data inserted successfully!"); // 성공 메시지 전송
+  });
+});
+
 app.listen(PORT, () => {
   console.log(`running on port ${PORT}`);
 });
